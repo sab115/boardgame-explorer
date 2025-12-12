e37501777da52eba21ee285bc570aa56be76e7c6
sab115 <shuan115@calpoly.edu>
2025-12-13 06:13:42 +0800

    fix: vite config

--- packages/app/vite.config.js
+++ packages/app/vite.config.js
@@ -1,9 +1,21 @@
-export default {
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
     server: {
         proxy: {
             "/api": "http://localhost:3000",
             "/auth": "http://localhost:3000",
             "/images": "http://localhost:3000"
         }
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, 'login.html'),
+                register: resolve(__dirname, 'register.html')
+            }
+        }
     }
-};
+});
